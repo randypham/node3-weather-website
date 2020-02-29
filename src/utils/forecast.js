38fc5376c713bec635e219dd3593e5657415ebf9
Randy Pham <randypham50@gmail.com>
2020-02-29 15:45:42 -0500

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,27 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    'https://api.darksky.net/forecast/66e60b7a5671a4bd03efc781fc54757e/' +
+    latitude +
+    ',' +
+    longitude;
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect to weather service!', undefined);
+    } else if (body.error) {
+      callback('Unable to find location!', undefined);
+    } else {
+      callback(
+        undefined,
+        body.daily.data[0].summary +
+          ' Its currently ' +
+          body.currently.temperature +
+          ' degrees out.'
+      );
+    }
+  });
+};
+
+module.exports = forecast;
